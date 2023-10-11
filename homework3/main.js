875d9fd42599e6859cb98cb8d8ba76f98bf532b9
Sanjar <115815477+lifeofsanjar@users.noreply.github.com>
2023-10-11 12:45:25 +0500

    Add files via upload

---
+++ homework3/main.js
@@ -0,0 +1,119 @@
+//for1
+
+// let n,k
+// k=parseInt(prompt("sonni kiriting:"))
+// n=parseInt(prompt("nechchi marta chiqarilsin:"))
+// for(let i=0 ;i<n;i++)
+// {
+//     alert(i+1+"n"+k)
+// }
+
+//for2
+
+// let a,b,i
+// a=parseInt(prompt("a"))
+// b=parseInt(prompt("b"))
+// for(i=a;i<=b;i++)
+// {
+//     alert(i)
+// }
+
+//for3
+
+// let a,b,i
+// a=parseInt(prompt("a"))
+// b=parseInt(prompt("b"))
+// for(i=a;i>=b;i--)
+// {
+//     alert(i)
+// }
+
+//for4
+
+// let price
+// price=parseFloat(prompt("Konfetning narxini kiriting:"))
+// for(let i=1;i<=10;i++)
+// {
+//     alert(i*price+" sum")
+// }
+
+//for5
+
+// let price
+// price=parseFloat(prompt("Konfetning narxini kiriting:"))
+// for(let i=1;i<=10;i++)
+// {
+//     alert((i/10)*price+" sum")
+// }
+
+//for6
+
+// let price
+// price=parseFloat(prompt("Konfetning narxini kiriting:"))
+// for(let i=1.2;i<=2;i=i+0.2)
+// {
+//     alert(i*price+" sum")
+// }
+
+//for7
+
+// let s=0,a,b
+// a=parseInt(prompt("a"))
+// b=parseInt(prompt("b"))
+// for(let i=a;i<=b;i++)
+// {
+//     s=s+i
+// }
+// alert(s)
+
+//for8
+
+// let s=1,a,b
+// a=parseInt(prompt("a"))
+// b=parseInt(prompt("b"))
+// for(let i=a;i<=b;i++)
+// {
+//     s=s*i
+// }
+// alert(s)
+
+//for9
+
+// let s=0,a,b
+// a=parseInt(prompt("a"))
+// b=parseInt(prompt("b"))
+// for(let i=a;i<=b;i++)
+// {
+//     s=s+Math.pow(i,2)
+// }
+// alert(s)
+
+//for10
+
+// let s=0,n
+// n=parseInt(prompt("n"))
+// for(let i=1;i<=n;i++)
+// {
+//     s=s+(i/n)
+// }
+// alert(s)
+
+//for11
+
+// let s=0,n
+// n=parseInt(prompt("n"))
+// for(let i=0;i<=n;i++)
+// {
+//     s=s+(n+i)**2
+// }
+// alert(s)
+
+//for12
+
+let s=1,n
+n=parseInt(prompt("n"))
+for(let i=1;i<=n;i++)
+{
+    s=s*((i+i/10)-(i-1))
+}
+alert(s)
